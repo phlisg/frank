491e6b94ee09b90e8beb24f2852caa50bd217bd9
Phlisg <phlisg@proton.me>
2026-05-01 20:42:58 +0200

    feat: added goreleaser

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ import (
 //go:embed all:templates
 var templateFS embed.FS
 
+var version = "dev"
+
 func main() {
-	version := "dev"
-	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
-		version = info.Main.Version
+	if version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		}
 	}
 	cmd.Execute(templateFS, version)
 }
